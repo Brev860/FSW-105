8f9bdb41c4d62229ded677394bd5b37e81d25e55
Brev860 <brevan.daniels@bryanuniversity.edu>
2020-02-12 12:36:53 -0500

    first assignment

---
+++ week1/grocery_storeJS/assignment.js
@@ -0,0 +1,30 @@
+
+var guestShopper = {
+
+  nameFirst: "John",
+  nameLast: "Smith",
+  streetName: "Main st.",
+  streetNumber: 546,
+  city: "Hartford",
+  zipeCode: 06016,
+  stateProvence: "CT",
+  groceryCart: ["apples", "oranges", "milk", "eggs", "butter", "cheese", "bread"],
+
+};
+
+
+var getShopperName ={
+   myFunction: function(){
+           return guestShopper.nameFirst + " " + guestShopper.nameLast
+    }
+}
+console.log(getShopperName.myFunction());
+  
+
+
+
+
+
+
+
+

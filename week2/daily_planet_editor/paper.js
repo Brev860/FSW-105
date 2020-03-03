6bb00d9ab766298bfb311372ffd2ac2c18c6bf9e
Brev860 <brevan.daniels@bryanuniversity.edu>
2020-03-03 10:25:51 -0500

    resubmit

--- week2/daily_planet_editor/paper.js
+++ week2/daily_planet_editor/paper.js
@@ -9,48 +9,46 @@ just fix the existing ones.
 
 // Written by Kent, Clark
 
-var enemyName=["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
+var enemies=["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
 
-function whoWins(isThereKryptonite,enemyName)
+function whoWins(isThereryKptonite,enemyName)
 {
-  if(isThereKryptonite) 
-  {
-return ("Superman beats " +enemyName+ ", of course");
-}  else {
-    return ("Depends on how quick Superman can get rid of the Kryptonite. "+ enemyName +" could possibly win this one.");
-}
-    
-}
-for (var i=0;i<enemyName.length;i++)
+if (!isThereKryptonite)
 {
-    var isThereKryptonite
-    if(i%2=== 0) {
+    return "Superman beats " +enemyName+ ", of course";
+}
+
+else{
+    return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyName +" could possibly win this one.";
+}
+    }
+
+for (var i=0;i<enemies.length;i++)
+{
+    var isThereKryptonite 
+    if (i%2=== 0) {
         isThereKryptonite = true;
     } else {
         isThereKryptonite = false;
     }
-    console.log(whoWins(isThereKryptonite,enemyName[i]));
-}
-var math = 2
-function howAttractedIsLoisLaneToMe()
-{
-    // 1 is not at all attracted, 10 is "super" attracted...
-    return math.floor * math.random * 10+1;
+    console.log(whoWins(isThereKryptonite,enemies[i]));
 }
 
-console.log(howAttractedIsLoisLaneToMe()) ;
+function howAttractedIsLoisLaneToMe ()
+{
+    // 1 is not at all attracted, 10 is "super" attracted...
+    return Math.floor((Math.random()*10)+1);
+}
+
+console.log( howAttractedIsLoisLaneToMe ()) ;
 
 var clarkKent = true;
 var superman = false;
 
 while (clarkKent){
     console.log("I'm just a nerdy columnist");
-}
-    var phoneBoothQuickChange =math.random ;
-    { 
-    if (phoneBoothQuickChange>= 0.5) 
+    var phoneBoothQuickChange =Math.random();
+    if (phoneBoothQuickChange>= 0.5) {
         clarkKent=false;
         superman=true;
-
-        console.log("Now I'm Superman!")
-    }
+        console.log("Now I'm Superman!");}}
